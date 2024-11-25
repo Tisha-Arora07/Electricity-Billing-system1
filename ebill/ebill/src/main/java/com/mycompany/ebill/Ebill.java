351740a6176e2c19c4ead26579d96bafa5df1af6
Tisha-Arora07 <132424384+Tisha-Arora07@users.noreply.github.com>
2024-11-25 16:31:39 +0530

    Add files via upload

---
+++ ebill/ebill/src/main/java/com/mycompany/ebill/Ebill.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ebill;
+
+/**
+ *
+ * @author hp
+ */
+public class Ebill {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
